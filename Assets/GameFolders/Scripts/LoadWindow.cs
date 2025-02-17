35c2ceac2a42ef8fe34acce731fe27fcbebbe113
Alexander Evstratov <alexdrugev@me.com>
2025-02-17 12:43:58 +0300

    Add load scene.

---
+++ Assets/GameFolders/Scripts/LoadWindow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadWindow : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(OpenMainScene());
+    }
+
+    private IEnumerator OpenMainScene()
+    {
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
